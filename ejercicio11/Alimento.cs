130237684464827bf6b19626c452073c53641dd2
Orlando <orlandocursocic@gmail.com>
2017-07-12 16:51:38 +0200

    Añadido metodo equals (a modo temporal de aprendizaje) en la clase Alimento

--- ejercicio11/Alimento.cs
+++ ejercicio11/Alimento.cs
@@ -9,10 +9,38 @@ namespace ejercicio11
     public class Alimento
     {
         public double peso { get; set; }
+        public string nombre { get; set; }
 
         public Alimento(double peso)
         {
             this.peso = peso;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            if (((Alimento)obj).nombre == null)
+            {
+                if (this.nombre != null)
+                    return false;
+            }
+            else
+            {
+                if (!((Alimento)obj).nombre.Equals(this.nombre))
+                {
+                    return false;
+                }
+            }
+            return ((Alimento)obj).peso == this.peso;
         }
 
     }
